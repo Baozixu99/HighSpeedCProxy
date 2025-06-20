e0e1f14c9f2f3607dbce17cad82ab3565178389c
lr <lr@gitee.com>
2025-06-20 10:42:42 +0800

    add eventloop

--- include/netns_socket.h
+++ include/netns_socket.h
@@ -1,7 +1,8 @@
 #ifndef NETNS_SOCKET_H
 #define NETNS_SOCKET_H
 
-
 int create_socket_netns(int ns_fd);
+void set_nonblocking(int sockfd);
+void add_socket_to_epoll(int epoll_fd, int sock_fd, uint32_t events);
 
 #endif /* NETNS_SOCKET_H */
--- src/main.c
+++ src/main.c
@@ -1,13 +1,94 @@
 
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
 #include "netns_socket.h"
 #include "dev.h"
 #include "session.h"
 #include "session_pool.h"
 #include "channel.h"
 
+#define MAX_EVENTS 10
+#define BUFFER_SIZE 1024
+
 void print_pool(struct BackendSessionPool* s_pool);
 void high_speed_delete_all_sess(struct BackendSessionPool* s_pool);
+
+void epoll_run(int epoll_fd)
+{
+    while (1)
+    {
+        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (nfds == -1) 
+        {
+            printf("epoll_wait:no ready fds!\n");
+            continue;
+        }
+        for (int i = 0; i < nfds; i++) {
+            int sockfd = events[i].data.fd;
+            
+            if (events[i].events & EPOLLERR) 
+            {
+                int error = 0;
+                socklen_t len = sizeof(error);
+                if( getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len) == 0)
+                {
+                    printf("Socket %d error: %s\n", sockfd, strerror(error));
+                }
+                
+                //todo,remove sockfd and close session
+                // epoll_ctl(epoll_fd, EPOLL_CTL_DEL, sockfd, NULL);
+                close(sockfd);
+            }
+
+            if (events[i].events & EPOLLOUT) 
+            {
+                int error = 0;
+                socklen_t len = sizeof(error);
+                getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len);
+                if (error == 0) {
+                    printf("Connected to server via socket %d\n", sockfd);
+                    
+                    //todo, send data to outside
+                    const char *msg = "Hello Server";
+                    send(sockfd, msg, strlen(msg), 0);
+
+                    struct epoll_event ev;
+                    ev.events = EPOLLIN | EPOLLET;
+                    ev.data.fd = sockfd;
+                    epoll_ctl(epoll_fd, EPOLL_CTL_MOD, sockfd, &ev);
+                } else {
+                    printf("Connection failed: %s\n", strerror(error));
+                    //todo,remove sockfd and close session
+                    close(sockfd);
+                }
+            }
+
+            if (events[i].events & EPOLLIN) 
+            {
+                char buf[BUFFER_SIZE];
+                ssize_t count = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
+                if (count > 0) 
+                {
+                    buf[count] = '\0';
+                    //todo, recv data to front
+                    printf("Received from %d: %s\n", sockfd, buf);
+                } else if (count == 0) { 
+                    printf("Server closed connection\n");
+                    //todo,remove sockfd and close session
+                    close(sockfd);
+                } else {
+                    if (errno != EAGAIN && errno != EWOULDBLOCK) 
+                    {
+                        //todo,remove sockfd and close session
+                        close(sockfd);
+                    }
+                }
+            }
+        }
+    }     
+}
+
 
 int main(int argc, char** argv)
 {
