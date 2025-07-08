61cc48d7014a24635b4b39b88f7cabd111a346f7
Your Name <you@example.com>
2025-07-08 13:38:51 +0800

    add macros for developing backend proxy stack

--- include/backend_proto.h
+++ include/backend_proto.h
@@ -1,7 +1,21 @@
 #ifndef BACKEND_PROTO_H
 #define BACKEND_PROTO_H
 
+#include "message.h"
 
+int proxy_msg_prosess(uint8_t *msg);
+int proxy_msg_response(uint8_t *msg);
 
+int proxy_dev_msg_prosess(uint8_t *msg);
+int proxy_dev_msg_response(uint8_t *msg);
+
+int proxy_dev_msg_prosess(uint8_t *msg);
+int proxy_dev_msg_response(uint8_t *msg);
+
+int proxy_strgy_msg_process(uint8_t *msg);
+int proxy_strgy_msg_response(uint8_t *msg);
+
+int proxy_sess_msg_process(uint8_t *msg);
+int proxy_sess_msg_response(uint8_t *msg);
 
 #endif
