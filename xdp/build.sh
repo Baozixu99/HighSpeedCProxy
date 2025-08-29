41a73f5e0c6384770831cf9e016a0c6d15dde1c4
lr <lr@gitee.com>
2025-08-29 14:26:34 +0800

    modify to static compile libbpf.a

--- xdp/build.sh
+++ xdp/build.sh
@@ -1,9 +1,8 @@
 # clang -O2 -g -Wall -target bpf -c xdp_redirect_sock_kern.c -o xdp_redirect.o
-aarch64-linux-gnu-gcc -o xdp_redirect_user xdp_redirect_sock_user.c \
+aarch64-linux-gnu-gcc -o xdp_redirect_user xdp_redirect_sock_user.c /home/h/libbpf-arm64/usr/lib/aarch64-linux-gnu/libbpf.a \
                       -I/home/h/libbpfdev-arm64/usr/include \
                       -L/home/h/libbpf-arm64/usr/lib/aarch64-linux-gnu -lelf \
                       -L/home/h/libbpf-arm64/usr/lib/aarch64-linux-gnu -lz \
-                      -L/home/h/libbpf-arm64/usr/lib/aarch64-linux-gnu -lbpf
                       
 
 # gcc -o xdp_redirect_user xdp_redirect_sock_user.c -lbpf
