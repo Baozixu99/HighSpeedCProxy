69c89e6b9f50948e87e911a7e638adac5b360dc4
lr <lr@gitee.com>
2026-03-12 15:01:25 +0800

    modify build to relative path

--- bt/build.sh
+++ bt/build.sh
@@ -1,7 +1,7 @@
 aarch64-none-linux-gnu-gcc -o bt_client bt_client.c \
-                      -I/home/h/HighSpeedCProxy/lib/bt/bluez/usr/include \
-                      -L/home/h/HighSpeedCProxy/lib/bt/bluez/usr/lib -lbluetooth
+                      -I../lib/bt/bluez/usr/include \
+                      -L../lib/bt/bluez/usr/lib -lbluetooth
 
 aarch64-none-linux-gnu-gcc -o bt_server bt_server.c \
-                      -I/home/h/HighSpeedCProxy/lib/bt/bluez/usr/include \
-                      -L/home/h/HighSpeedCProxy/lib/bt/bluez/usr/lib -lbluetooth
+                      -I../lib/bt/bluez/usr/include \
+                      -L../lib/bt/bluez/usr/lib -lbluetooth
